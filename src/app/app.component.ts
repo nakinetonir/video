4e1d218990ac1667552ac7d1e9dfc9273b1d8247
Moneo Arau <imoneo@minsait.com>
2025-03-17 16:15:25 +0100

    d2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,5 +11,60 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'video';
+  @ViewChild('preview') previewVideoElement!: ElementRef;
+  videoStream!: MediaStream;
+  mediaRecorder!: MediaRecorder;
+  recordedChunks: Blob[] = [];
+  videoBlob!: Blob;
+  videoURL: string = '';
+  isRecording: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  async startCamera() {
+    try {
+      this.videoStream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: 'user', width: { ideal: 1280 }, height: { ideal: 720 } },
+        audio: true,
+      });
+      const previewVideo = this.previewVideoElement.nativeElement;
+      previewVideo.srcObject = this.videoStream;
+      previewVideo.play();
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
+  }
+
+  startRecording() {
+    this.recordedChunks = [];
+    this.mediaRecorder = new MediaRecorder(this.videoStream);
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        this.recordedChunks.push(event.data);
+      }
+    };
+    this.mediaRecorder.onstop = () => {
+      this.videoBlob = new Blob(this.recordedChunks, { type: 'video/webm' });
+      this.videoURL = URL.createObjectURL(this.videoBlob);
+      this.cdr.detectChanges();
+    };
+    this.mediaRecorder.start();
+    this.isRecording = true;
+  }
+
+  stopRecording() {
+    if (this.mediaRecorder) {
+      this.mediaRecorder.stop();
+      this.isRecording = false;
+    }
+  }
+
+  downloadVideo() {
+    const a = document.createElement('a');
+    a.href = this.videoURL;
+    a.download = 'recorded-video.webm';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
 }
